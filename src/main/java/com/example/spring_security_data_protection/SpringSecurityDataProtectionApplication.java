6ed25b75fa965fd60f1bc29ec2624eb82731d86b
Supun Kavinda <naskavinda@gmail.com>
2024-08-01 00:25:07 +0530

    initial commit

---
+++ src/main/java/com/example/spring_security_data_protection/SpringSecurityDataProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_data_protection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDataProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDataProtectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_security_data_protection/SpringSecurityDataProtectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_data_protection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDataProtectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
